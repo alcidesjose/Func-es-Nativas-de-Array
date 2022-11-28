44fbf3fe36477c1c758fb4dc5a34de154b527c3f
JUNIOR <iqalcides@gmail.com>
2022-11-28 17:30:37 -0300

    Novo arquivo

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+// $lista = ['nome1', 'nome2','nome3','nome4','nomes5'];
+
+// echo 'Total: '.count($lista);  // retorna a quantidade itens de uma lista
+
+// $lista2 = ['Cides', 'Pedro', 'Paulo', 'Jose', 'Francisca', 'Maria'];
+
+// $aprovados =['Cides', 'Pedro', 'Jose', 'Francisca'];
+
+// $reprovados = array_diff($lista2,$aprovados); //array_diff gera a condição descrita a ele nesse caso ele retorna o s aprovados
+
+// print_r($reprovados);
+
+// $numeros = [10 ,20 , 24, 91, 18];
+
+// $filtrados = array_filter($numeros, function($item){
+//  if($item <br 30){
+//   return true;
+//  }else {
+//   return false;
+//  }
+// });  //array_filter executa a função designada no bloco e retorna 'true' ou   'false'
+
+// print_r($filtrados);
+
+// $dobrados = array_map(function($item){
+
+//   return $item * 2;
+
+// },$numeros); //array_map percore todo o array e retorma a condiçãso descrita no bloco defunção, array_map ele não filtra apenas retorna os items percoridos dentro do array
+
+// print_r($dobrados);
+
+// array_pop($numeros);//remove o ultimo item do array
+
+// array_shift($numeros);//remove o primeiro item do array
+
+// array_push($numeros,96);// adiciona um item a ultima posição do array
+
+// print_r($numeros);
+//  echo '</br>';
+
+// if(in_array(91,$numeros)) {
+//   echo 'existe';
+
+// }else{
+//   echo '</br>';
+//   echo 'não existe';
+// }
+
+
+// $pos =array_search(14, $numeros);// search retorna a posição do item dentro do aaray se ele existir
+// echo $pos;
+
+//rsort($numeros);//oredna os itens dentro do array em ordem reversa(decrescente)
+
+// asort($numeros);// ordena os itens dentro do array mantendo a relação entre as chaves e os valores
+
+// echo 'A chave 3 é: '.$numeros[3];
+
+// //sort($numeros);// ordena os itens dentro array(crescente)
+
+// print_r($numeros);
+
+
+// $nome = 'Alcides Jose Costa';
+
+// $nomes = explode(' ',$nome);
+
+// print_r($nomes);
+
+$nomes = ['Alcides Jose Costa'];
+
+$nome = implode(' ',$nomes);
+
+echo $nome;
